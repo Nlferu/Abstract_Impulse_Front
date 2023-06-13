f2ce53ad2abe5984eead3e8bf1d0b84a30052e43
MarkizOrias <georgi.bargan@gmail.com>
2023-06-13 15:41:54 +0200

    smartphones up

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -4,13 +4,10 @@ import { useState, useEffect, useRef } from 'react'
 import Image from "next/image"
 import { formatAge } from './AuctionTimer'
 import brush from '../public/BRUSH3.png'
-import l1 from '../public/4.png'
-import l2 from '../public/5.png'
+import l1 from '../public/1.png'
+import l2 from '../public/2.png'
 import l3 from '../public/3.png'
-import l4 from '../public/6.png'
-import l5 from '../public/2.png'
-import l6 from '../public/7.png'
-import l7 from '../public/1.png'
+import l4 from '../public/4.png'
 
 const truncateStr = (fullStr, strLen) => {
     if (fullStr.length <= strLen) return fullStr
@@ -36,7 +33,7 @@ export default function NFTBox({ tokenId, claimedNfts, approvedNfts, setTokenURI
     const [status, setStatus] = useState('')
     const [isAuctionTimerZero, setIsAuctionTimerZero] = useState(Math.max(auctionTimer.blockTimestamp * 1000 + auctionTimer.time * 1000 - Date.now(), 0))
     const [imageLoading, setImageLoading] = useState(true)
-    const loadingImages = [l1, l2, l3, l4, l5, l6, l7]
+    const loadingImages = [l1, l2, l3, l4]
     const [currentImages, setCurrentImages] = useState([])
     const intervalRef = useRef(null)
 
