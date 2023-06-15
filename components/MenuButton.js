7b0707e2a871460c4a3fa93f9725b84f993199a1
MarkizOrias <georgi.bargan@gmail.com>
2023-06-16 00:13:02 +0200

    up

--- components/MenuButton.js
+++ components/MenuButton.js
@@ -1,8 +1,20 @@
-
+import { useState } from 'react'
+import styles from '@/styles/Menu.module.css'
 import menu from '../public/menu.png'
 import Image from "next/image"
+import Link from "next/link"
 
 export default function MenuButton(isSmartphoneViewEnabled) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleMenuList = () => {
+        setIsModalOpen(!isModalOpen);
+    }
+
+    const handleClose = () => {
+        setIsModalOpen(false);
+    }
+
     return (
         <div>
             <Image
@@ -12,8 +24,24 @@ export default function MenuButton(isSmartphoneViewEnabled) {
                 height={40}
                 objectFit="cover"
                 alt="menu icon"
+                onClick={handleMenuList}
             />
+
+            {isModalOpen && (
+                <div className={styles.modal}>
+                    <div>
+                        <Link href="/" onClick={handleClose} className={styles.homeButton}>
+                            <span>HOME</span>
+                        </Link>
+                        <Link href="/withdraw" onClick={handleClose} className={styles.homeButton}>
+                            <span>Withdraw</span>
+                        </Link>
+                        <Link href="/about" onClick={handleClose} className={styles.homeButton}>
+                            <span>About</span>
+                        </Link>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
-
