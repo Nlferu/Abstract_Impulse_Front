69b89f2909998692614dd223e413d3fe8ebe75b8
MarkizOrias <georgi.bargan@gmail.com>
2023-07-03 18:29:26 +0200

    about update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,33 +1,38 @@
 import styles from '@/styles/About.module.css'
+import Image from "next/image"
+import Jessica1 from '../../public/Jessica1.JPG'
 
 export default function About() {
     return (
         <div className={styles.container}>
-            <h1 className={`${styles.blockTitle} ${styles.glowTextEffect}`}>
-                HOW DOES IT WORK?
-            </h1>
-            <div className={styles.videoContainer}>
-                <iframe
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/we-mm6V6H00"
-                    title="YouTube Video"
-                    frameborder="0"
-                    allow="accelerometer autoplay clipboard-write encrypted-media gyroscope picture-in-picture"
-                    allowfullscreen
-                ></iframe>
+            <div className={styles.blockContainer}>
+                <h1 className={`${styles.blockTitle} ${styles.glowTextEffect}`}>
+                    ABOUT THE ARTIST
+                </h1>
+                <div className={styles.description}>
+                    <p>Painting is an intense moment of escape and expression. My palette knives are the guides for my emotions on the canvas. My paintings are a reflection of what is within me, that which I can only express through the material, colors, and abstraction.</p>
+                    <div className={styles.image}>
+                        <Image
+                            src={Jessica1}
+                        />
+                    </div>
+                </div>
             </div>
-            <h1 className={`${styles.blockTitle} ${styles.glowTextEffect}`}>
-                ABOUT THE ARTIST
-            </h1>
-            <div>
-                <p>This will be the description of the artist</p>
-                <p>This will be the description of the artist</p>
-                <p>This will be the description of the artist</p>
-                <p>This will be the description of the artist</p>
-                <p>This will be the description of the artist</p>
-                <p>This will be the description of the artist</p>
-                <p>This will be the description of the artist</p>
+            <div className={styles.blockContainer}>
+                <h1 className={`${styles.blockTitle} ${styles.glowTextEffect}`}>
+                    HOW DOES IT WORK?
+                </h1>
+                <div className={styles.videoContainer}>
+                    <iframe
+                        width="900"
+                        height="600"
+                        src="https://www.youtube.com/embed/we-mm6V6H00"
+                        title="YouTube Video"
+                        frameborder="0"
+                        allow="accelerometer autoplay clipboard-write encrypted-media gyroscope picture-in-picture"
+                        allowfullscreen
+                    ></iframe>
+                </div>
             </div>
         </div>
     )
