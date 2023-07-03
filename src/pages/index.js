3ddfab94bcfd3571b6ddfb340842e82199d52010
MarkizOrias <georgi.bargan@gmail.com>
2023-07-03 10:38:05 +0200

    upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ import NFTBox from "../../components/NFTBox"
 import BlockingLayer from "../../components/BlockingLayer"
 import BlackoutLayer from "../../components/BlackoutLayer"
 import BiddingModal from "../../components/BiddingModal"
-import { useRouter } from 'next/router';
-import UAParser from 'ua-parser-js';
+import { useRouter } from 'next/router'
+import UAParser from 'ua-parser-js'
 
 export default function Home() {
 
@@ -18,7 +18,7 @@ export default function Home() {
   const [isDesktopView, setIsDesktopView] = useState(false)
   const [swipedUp, setSwipedUp] = useState(false)
   const [disabledCardsIndexes, setDisabledCardsIndexes] = useState([])
-  const router = useRouter();
+  const router = useRouter()
 
   const swipeHandlers = useSwipeable({
     onSwipedLeft: () => isDesktopView === false && handleNextCard(),
@@ -42,14 +42,14 @@ export default function Home() {
   }, [])
 
   useEffect(() => {
-    const parser = new UAParser();
-    const result = parser.getResult();
-    const browserName = result.browser.name;
+    const parser = new UAParser()
+    const result = parser.getResult()
+    const browserName = result.browser.name
 
     if (browserName !== 'Chrome') {
-      router.push('/unsupported');
+      router.push('/unsupported')
     }
-  }, []);
+  }, [])
 
   if (isDesktopView === null) return null
 
