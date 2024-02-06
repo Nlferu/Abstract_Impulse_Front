56825e805fa9930a838176866c2035fda5fb0bba
Niferu <wargacki.patryq@gmail.com>
2024-02-06 01:12:26 +0100

    useEffect, .env removed

--- components/Header.js
+++ components/Header.js
@@ -1,62 +1,58 @@
-import { ConnectButton } from "web3uikit"
-import Link from "next/link"
-import styles from '@/styles/Header.module.css'
-import { useState, useEffect, useLayoutEffect } from "react"
-import MenuButton from './MenuButton'
+import { ConnectButton } from "web3uikit";
+import Link from "next/link";
+import styles from "@/styles/Header.module.css";
+import { useState, useEffect, useLayoutEffect } from "react";
+import MenuButton from "./MenuButton";
 
 export default function Header() {
-    const [isDesktopView, setIsDesktopView] = useState(null)
+  const [isDesktopView, setIsDesktopView] = useState(null);
 
-    useLayoutEffect(() => {
-        function updateSize() {
-            setIsDesktopView(window.innerWidth > 600)
-        }
+  useEffect(() => {
+    function updateSize() {
+      setIsDesktopView(window.innerWidth > 600);
+    }
 
-        window.addEventListener('resize', updateSize)
-        updateSize() // Call this function immediately to update state with initial window size.
+    window.addEventListener("resize", updateSize);
+    updateSize(); // Call this function immediately to update state with initial window size.
 
-        return () => window.removeEventListener('resize', updateSize)
-    }, [])
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
 
-    if (isDesktopView === null) return null
+  if (isDesktopView === null) return null;
 
-    return (
-        <header className={styles.header}>
-            <nav className={styles.navigationBar}>
-                <Link href="/">
-                    <h1 className={styles.headerOne}>ABSTRACT IMPULSE NFT</h1>
-                </Link>
-                <div className={styles.navObj}>
-                    {isDesktopView && (
-                        <div>
-                            <Link href="/" className={styles.homeButton}>
-                                <span>HOME</span>
-                            </Link>
-                            <Link href="/withdraw" className={styles.homeButton}>
-                                <span>Withdraw</span>
-                            </Link>
-                            <Link href="/about" className={styles.homeButton}>
-                                <span>About</span>
-                            </Link>
-                        </div>
-                    )}
-                    <div className={styles.connButton}>
-                        <ConnectButton
-                            moralisAuth={false}
-                        />
-                    </div>
-                    {!isDesktopView && (
-                        <div className={styles.menuButton}>
-                            <MenuButton
-                                key={setIsDesktopView}
-                                isSmartphoneViewEnabled={setIsDesktopView}
-                            />
-                        </div>
-                    )}
-                </div>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <nav className={styles.navigationBar}>
+        <Link href="/">
+          <h1 className={styles.headerOne}>ABSTRACT IMPULSE NFT</h1>
+        </Link>
+        <div className={styles.navObj}>
+          {isDesktopView && (
+            <div>
+              <Link href="/" className={styles.homeButton}>
+                <span>HOME</span>
+              </Link>
+              <Link href="/withdraw" className={styles.homeButton}>
+                <span>Withdraw</span>
+              </Link>
+              <Link href="/about" className={styles.homeButton}>
+                <span>About</span>
+              </Link>
+            </div>
+          )}
+          <div className={styles.connButton}>
+            <ConnectButton moralisAuth={false} />
+          </div>
+          {!isDesktopView && (
+            <div className={styles.menuButton}>
+              <MenuButton
+                key={setIsDesktopView}
+                isSmartphoneViewEnabled={setIsDesktopView}
+              />
+            </div>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
 }
-
-
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
-import { useQuery } from "@apollo/client"
-import styles from '@/styles/Home.module.css'
-import { useState, useEffect, useLayoutEffect } from 'react'
-import { useSwipeable } from 'react-swipeable'
-import GET_ACTIVE_ITEMS from "../../constants/subgraphQueries"
-import NFTBox from "../../components/NFTBox"
-import BlockingLayer from "../../components/BlockingLayer"
-import BlackoutLayer from "../../components/BlackoutLayer"
-import BiddingModal from "../../components/BiddingModal"
+import { useQuery } from "@apollo/client";
+import styles from "@/styles/Home.module.css";
+import { useState, useEffect, useLayoutEffect } from "react";
+import { useSwipeable } from "react-swipeable";
+import GET_ACTIVE_ITEMS from "../../constants/subgraphQueries";
+import NFTBox from "../../components/NFTBox";
+import BlockingLayer from "../../components/BlockingLayer";
+import BlackoutLayer from "../../components/BlackoutLayer";
+import BiddingModal from "../../components/BiddingModal";
 
 export default function Home() {
-
-  const { loading, error, data } = useQuery(GET_ACTIVE_ITEMS)
-  const [isBiddingModalOpen, setIsBiddingModalOpen] = useState(false)
-  const [isTransactionOpen, setIsTransactionOpen] = useState(false)
-  const [isDesktopView, setIsDesktopView] = useState(false)
-  const [swipedUp, setSwipedUp] = useState(false)
-  const [disabledCardsIndexes, setDisabledCardsIndexes] = useState([])
+  const { loading, error, data } = useQuery(GET_ACTIVE_ITEMS);
+  const [isBiddingModalOpen, setIsBiddingModalOpen] = useState(false);
+  const [isTransactionOpen, setIsTransactionOpen] = useState(false);
+  const [isDesktopView, setIsDesktopView] = useState(false);
+  const [swipedUp, setSwipedUp] = useState(false);
+  const [disabledCardsIndexes, setDisabledCardsIndexes] = useState([]);
 
   const swipeHandlers = useSwipeable({
     onSwipedLeft: () => isDesktopView === false && handleNextCard(),
@@ -24,126 +23,151 @@ export default function Home() {
     onSwipedDown: () => isDesktopView === false && setSwipedUp(false),
     preventDefaultTouchmoveEvent: true,
     trackMouse: true,
-  })
-
-  useLayoutEffect(() => {
-    function updateSize() {
-      setIsDesktopView(window.innerWidth > 600)
-      setSwipedUp(window.innerWidth > 600)
-    }
-
-    window.addEventListener('resize', updateSize)
-    updateSize() // Call this function immediately to update state with initial window size.
-
-    return () => window.removeEventListener('resize', updateSize)
-  }, [])
-
-  if (isDesktopView === null) return null
-
-  const getFirstValidIndex = () => {
-    let index = 0
-    while (disabledCardsIndexes.includes(index)) {
-      index++
-    }
-    return index
-  }
-
-  const [currentCardIndex, setCurrentCardIndex] = useState(() => {
-    if (typeof window !== 'undefined') {
-      const savedIndex = localStorage.getItem('currentCardIndex')
-      if (savedIndex !== null && !disabledCardsIndexes.includes(Number(savedIndex))) {
-        return Number(savedIndex)
-      } else {
-        return getFirstValidIndex()
-      }
-    } else {
-      return getFirstValidIndex()
-    }
-  })
+  });
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('currentCardIndex', currentCardIndex)
+    function updateSize() {
+      setIsDesktopView(window.innerWidth > 600);
+      setSwipedUp(window.innerWidth > 600);
     }
-  }, [currentCardIndex])
 
+    window.addEventListener("resize", updateSize);
+    updateSize(); // Call this function immediately to update state with initial window size.
 
-  if (loading) return (<div className={styles.loadingPage}>Loading... Please wait.</div>)
-  if (error) return `Error! ${error.message}`
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
 
-  const hasNFTs = data.nftMinteds.length > 0
+  if (isDesktopView === null) return null;
+
+  const getFirstValidIndex = () => {
+    let index = 0;
+    while (disabledCardsIndexes.includes(index)) {
+      index++;
+    }
+    return index;
+  };
+
+  const [currentCardIndex, setCurrentCardIndex] = useState(() => {
+    if (typeof window !== "undefined") {
+      const savedIndex = localStorage.getItem("currentCardIndex");
+      if (
+        savedIndex !== null &&
+        !disabledCardsIndexes.includes(Number(savedIndex))
+      ) {
+        return Number(savedIndex);
+      } else {
+        return getFirstValidIndex();
+      }
+    } else {
+      return getFirstValidIndex();
+    }
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("currentCardIndex", currentCardIndex);
+    }
+  }, [currentCardIndex]);
+
+  if (loading)
+    return <div className={styles.loadingPage}>Loading... Please wait.</div>;
+  if (error) return `Error! ${error.message}`;
+
+  const hasNFTs = data.nftMinteds.length > 0;
 
   const handleNextCard = () => {
-    let newIndex = currentCardIndex
+    let newIndex = currentCardIndex;
     do {
-      newIndex = (newIndex + 1) % data.nftMinteds.length
-    } while (disabledCardsIndexes.includes(newIndex))
-    setCurrentCardIndex(newIndex)
-  }
+      newIndex = (newIndex + 1) % data.nftMinteds.length;
+    } while (disabledCardsIndexes.includes(newIndex));
+    setCurrentCardIndex(newIndex);
+  };
   const handlePrevCard = () => {
-    let newIndex = currentCardIndex
+    let newIndex = currentCardIndex;
     do {
-      newIndex = (newIndex - 1 + data.nftMinteds.length) % data.nftMinteds.length
-    } while (disabledCardsIndexes.includes(newIndex))
-    setCurrentCardIndex(newIndex)
-  }
+      newIndex =
+        (newIndex - 1 + data.nftMinteds.length) % data.nftMinteds.length;
+    } while (disabledCardsIndexes.includes(newIndex));
+    setCurrentCardIndex(newIndex);
+  };
 
-  const bullets = hasNFTs ? data.nftMinteds.map((_, i) => (
-    <div
-      key={i}
-      className={styles.bullet + (i === currentCardIndex ? ' ' + styles.active : '')}
-      style={disabledCardsIndexes.includes(i) ? { display: 'none' } : {}}
-      onClick={() => setCurrentCardIndex(i)}
-    />
-  )) : null
+  const bullets = hasNFTs
+    ? data.nftMinteds.map((_, i) => (
+        <div
+          key={i}
+          className={
+            styles.bullet + (i === currentCardIndex ? " " + styles.active : "")
+          }
+          style={disabledCardsIndexes.includes(i) ? { display: "none" } : {}}
+          onClick={() => setCurrentCardIndex(i)}
+        />
+      ))
+    : null;
 
-  const nftBidPlaceds = data.nftBidPlaceds
-  const nftAuctionTimeUpdateds = data.nftAuctionTimeUpdateds
+  const nftBidPlaceds = data.nftBidPlaceds;
+  const nftAuctionTimeUpdateds = data.nftAuctionTimeUpdateds;
 
-  const highestBids = {}
-  const highestBlockTimestamp = {}
+  const highestBids = {};
+  const highestBlockTimestamp = {};
 
   nftBidPlaceds.forEach((bid) => {
-    const tokenId = bid.tokenId
-    const amount = Number(bid.amount)
-    const bidder = bid.bidder
+    const tokenId = bid.tokenId;
+    const amount = Number(bid.amount);
+    const bidder = bid.bidder;
 
     if (!highestBids[tokenId] || amount > highestBids[tokenId].amount) {
-      highestBids[tokenId] = { amount, bidder }
+      highestBids[tokenId] = { amount, bidder };
     }
-  })
+  });
 
   nftAuctionTimeUpdateds.forEach((timer) => {
-    const tokenId = timer.tokenId
-    const time = timer.time
-    const blockTimestamp = timer.blockTimestamp
+    const tokenId = timer.tokenId;
+    const time = timer.time;
+    const blockTimestamp = timer.blockTimestamp;
 
-    if (!highestBlockTimestamp[tokenId] || blockTimestamp > highestBlockTimestamp[tokenId].blockTimestamp) {
-      highestBlockTimestamp[tokenId] = { blockTimestamp, time }
+    if (
+      !highestBlockTimestamp[tokenId] ||
+      blockTimestamp > highestBlockTimestamp[tokenId].blockTimestamp
+    ) {
+      highestBlockTimestamp[tokenId] = { blockTimestamp, time };
     }
-  })
+  });
 
   const resultBid = Object.entries(highestBids)
     .sort((a, b) => a[0] - b[0])
-    .map((entry) => ({ tokenId: entry[0], amount: entry[1].amount, bidder: entry[1].bidder }))
+    .map((entry) => ({
+      tokenId: entry[0],
+      amount: entry[1].amount,
+      bidder: entry[1].bidder,
+    }));
 
   const resultTime = Object.entries(highestBlockTimestamp)
     .sort((a, b) => a[0] - b[0])
-    .map((entry) => ({ tokenId: entry[0], blockTimestamp: entry[1].blockTimestamp, time: entry[1].time }))
+    .map((entry) => ({
+      tokenId: entry[0],
+      blockTimestamp: entry[1].blockTimestamp,
+      time: entry[1].time,
+    }));
 
   const approvedNfts = data.nftWithdrawCompleteds.reduce((acc, nft) => {
-    acc[nft.tokenId] = true
-    return acc
-  }, [])
-  if (approvedNfts[currentCardIndex]) { } else { approvedNfts[currentCardIndex] = false }
+    acc[nft.tokenId] = true;
+    return acc;
+  }, []);
+  if (approvedNfts[currentCardIndex]) {
+  } else {
+    approvedNfts[currentCardIndex] = false;
+  }
 
   const claimedNfts = data.transfers.reduce((acc, nft) => {
-    acc[nft.tokenId] = true
-    return acc
-  }, [])
-  if (claimedNfts[currentCardIndex]) { } else { claimedNfts[currentCardIndex] = false }
+    acc[nft.tokenId] = true;
+    return acc;
+  }, []);
+  if (claimedNfts[currentCardIndex]) {
+  } else {
+    claimedNfts[currentCardIndex] = false;
+  }
 
-  if (disabledCardsIndexes.includes(currentCardIndex)) return null
+  if (disabledCardsIndexes.includes(currentCardIndex)) return null;
 
   return (
     <div className={styles.container} {...swipeHandlers}>
@@ -162,7 +186,7 @@ export default function Home() {
             swipedUp={swipedUp}
             isDesktopView={isDesktopView}
           />
-          {isTransactionOpen && (<BlackoutLayer />)}
+          {isTransactionOpen && <BlackoutLayer />}
           {isBiddingModalOpen && (
             <>
               <BlockingLayer />
@@ -175,12 +199,30 @@ export default function Home() {
             </>
           )}
         </div>
-      ) : (<div className={styles.loadingPage}>No NFT has been minted yet!</div>)}
-      {isDesktopView && <div className={styles.arrow + ' ' + styles.left} onClick={handlePrevCard}>{'<'}</div>}
-      {isDesktopView && <div className={styles.arrow + ' ' + styles.right} onClick={handleNextCard}>{'>'}</div>}
+      ) : (
+        <div className={styles.loadingPage}>No NFT has been minted yet!</div>
+      )}
+      {isDesktopView && (
+        <div
+          className={styles.arrow + " " + styles.left}
+          onClick={handlePrevCard}
+        >
+          {"<"}
+        </div>
+      )}
+      {isDesktopView && (
+        <div
+          className={styles.arrow + " " + styles.right}
+          onClick={handleNextCard}
+        >
+          {">"}
+        </div>
+      )}
       {hasNFTs ? (
         <div className={styles.bulletsContainer}>{bullets}</div>
-      ) : (<div className={styles.loadingPage}>No NFT has been minted yet!</div>)}
+      ) : (
+        <div className={styles.loadingPage}>No NFT has been minted yet!</div>
+      )}
     </div>
-  )
+  );
 }
