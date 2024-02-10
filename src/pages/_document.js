d4c7cb98c19862cd62d276dc2411fc0f7bb3563f
Niferu <wargacki.patryq@gmail.com>
2024-02-10 18:36:19 +0100

    variable error fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Braah+One&family=Chakra+Petch:ital,wght@0,500;1,500&family=Gruppo&family=Press+Start+2P&family=Sofia+Sans+Condensed:wght@200&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Braah+One&family=Chakra+Petch:ital,wght@0,500;1,500&family=Gruppo&family=Press+Start+2P&family=Sofia+Sans+Condensed:wght@200&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
