336c65e449a3721f6d45f43d3b28a4348f95ee38
Niferu <wargacki.patryq@gmail.com>
2024-02-12 13:07:23 +0100

    unused var removed

--- components/MenuButton.js
+++ components/MenuButton.js
@@ -1,49 +1,61 @@
-import { useState } from 'react'
-import styles from '@/styles/Menu.module.css'
-import Link from "next/link"
+import { useState } from "react";
+import styles from "@/styles/Menu.module.css";
+import Link from "next/link";
 
-export default function MenuButton(isSmartphoneViewEnabled) {
-    const [isModalOpen, setIsModalOpen] = useState(false)
-    const [isOpen, setIsOpen] = useState(false)
+export default function MenuButton() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleMenuList = () => {
-        setIsModalOpen(!isModalOpen)
-    }
+  const handleMenuList = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
-    const handleClose = () => {
-        setIsModalOpen(false)
-    }
+  const handleClose = () => {
+    setIsModalOpen(false);
+  };
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen)
-    }
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div className={styles.menuButton}>
-            <div
-                className={`${styles.hamburgerMenu} ${isOpen && isModalOpen ? styles.open : ''}`}
-                onClick={() => { toggleMenu(), handleMenuList() }}
+  return (
+    <div className={styles.menuButton}>
+      <div
+        className={`${styles.hamburgerMenu} ${
+          isOpen && isModalOpen ? styles.open : ""
+        }`}
+        onClick={() => {
+          toggleMenu(), handleMenuList();
+        }}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {isModalOpen && (
+        <div className={styles.modal}>
+          <div>
+            <Link href="/" onClick={handleClose} className={styles.homeButton}>
+              <span>HOME</span>
+            </Link>
+            <Link
+              href="/withdraw"
+              onClick={handleClose}
+              className={styles.homeButton}
             >
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-
-            {isModalOpen && (
-                <div className={styles.modal}>
-                    <div>
-                        <Link href="/" onClick={handleClose} className={styles.homeButton}>
-                            <span>HOME</span>
-                        </Link>
-                        <Link href="/withdraw" onClick={handleClose} className={styles.homeButton}>
-                            <span>Withdraw</span>
-                        </Link>
-                        <Link href="/about" onClick={handleClose} className={styles.homeButton}>
-                            <span>About</span>
-                        </Link>
-                    </div>
-                </div>
-            )}
+              <span>Withdraw</span>
+            </Link>
+            <Link
+              href="/about"
+              onClick={handleClose}
+              className={styles.homeButton}
+            >
+              <span>About</span>
+            </Link>
+          </div>
         </div>
-    )
+      )}
+    </div>
+  );
 }
